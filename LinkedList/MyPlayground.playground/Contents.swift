dd539212f89046df83d02e53dbc4eca1e1ef1665
Eric Jang <940709@gmail.com>
2020-12-31 22:24:31 +0900

    Add example of node operation

--- LinkedList/MyPlayground.playground/Contents.swift
+++ LinkedList/MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
-
-var str = "Hello, playground"
+example(of: "printing linked nodes") {
+    let node1 = Node(value: 1)
+    let node2 = Node(value: 2)
+    let node3 = Node(value: 3)
+    
+    node1.next = node2
+    node2.next = node3
+    
+    print(node1)
+}
