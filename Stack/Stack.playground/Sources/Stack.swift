b62065e9f00b8e6dd703c11d422a0c07f0c0cab4
Eric Jang <940709@gmail.com>
2021-01-03 15:29:47 +0900

    Add stack practice file

--- Stack/Stack.playground/Sources/Stack.swift
+++ Stack/Stack.playground/Sources/Stack.swift
@@ -1,40 +1,40 @@
-import Foundation
-
-public struct Stack<Element> {
-    var storage = [Element]()
-    
-    public init() {}
-    
-    public init(_ elements: [Element]) {
-        storage = elements
-    }
-    
-    public mutating func push(_ element: Element) {
-        storage.append(element)
-    }
-    
-    @discardableResult
-    public mutating func pop() -> Element? {
-        storage.popLast()
-    }
-    
-    public func peek() -> Element? {
-        storage.last
-    }
-    
-    public var isEmpty: Bool {
-        peek() == nil
-    }
-}
-
-extension Stack: ExpressibleByArrayLiteral {
-    public init(arrayLiteral elements: Element...) {
-        storage = elements
-    }
-}
-
-extension Stack: CustomStringConvertible {
-    public var description: String {
-        return "\(storage.map { "\($0)"}.reversed().joined(separator: "\n"))"
-    }
-}
+//import Foundation
+//
+//public struct Stack<Element> {
+//    var storage = [Element]()
+//    
+//    public init() {}
+//    
+//    public init(_ elements: [Element]) {
+//        storage = elements
+//    }
+//    
+//    public mutating func push(_ element: Element) {
+//        storage.append(element)
+//    }
+//    
+//    @discardableResult
+//    public mutating func pop() -> Element? {
+//        storage.popLast()
+//    }
+//    
+//    public func peek() -> Element? {
+//        storage.last
+//    }
+//    
+//    public var isEmpty: Bool {
+//        peek() == nil
+//    }
+//}
+//
+//extension Stack: ExpressibleByArrayLiteral {
+//    public init(arrayLiteral elements: Element...) {
+//        storage = elements
+//    }
+//}
+//
+//extension Stack: CustomStringConvertible {
+//    public var description: String {
+//        return "\(storage.map { "\($0)"}.reversed().joined(separator: "\n"))"
+//    }
+//}
